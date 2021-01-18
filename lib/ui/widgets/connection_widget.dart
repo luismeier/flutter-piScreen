535ecedfba0c721384a2c95be3c1e80746cbaba2
Luis Meier <lmeier@hsr.ch>
2021-01-18 22:32:34 +0100

    Updated ConnectionWidget ui

--- lib/ui/widgets/connection_widget.dart
+++ lib/ui/widgets/connection_widget.dart
@@ -23,43 +23,58 @@ class ConnectionWidget extends StatelessWidget {
         border: Border.all(),
       ),
       child: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(1, 0, 10, 0),
-              child: Text(this.time, style: TextStyle(fontSize: 20)),
-            ),
-            this.depDelay == null
-                ? SizedBox(
-                    width: 40,
-                  )
-                : SizedBox(
-                    width: 40,
-                    child: Text(
-                      "$depDelay\'",
-                      style: TextStyle(
-                          color: Theme.of(context).errorColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
-                  ),
-            Container(
-              child: Text(
-                this.line,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              timeWidget(context),
+              SizedBox(width: 5),
+              SizedBox(
+                width: 28,
+                child: Text(
+                  this.line,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            SizedBox(width: 5),
-            Text(this.destination,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(width: 10),
-            Text("Gleis: $trackNumber", style: TextStyle(fontSize: 20))
-          ],
+              SizedBox(width: 5),
+              Expanded(
+                child: Text(
+                  this.destination,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                "Gl: $trackNumber",
+              )
+            ],
+          ),
         ),
-      )),
+      ),
+    );
+  }
+
+  Widget timeWidget(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          this.time,
+        ),
+        this.depDelay == null
+            ? Text("")
+            : Text(
+                "$depDelay\'",
+                style: TextStyle(
+                  color: Theme.of(context).errorColor,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+      ],
     );
   }
 }
